e4d43ed6795d3e48550999478a2df3b8d0af683b
Anna Myslyva <anna.myslyva@kpark.de>
2022-12-10 10:36:17 +0100

    My project: status on Sunday, 04.12.22

---
+++ src/code.js
@@ -0,0 +1,64 @@
+//Date + Time
+let now = new Date();
+function currentTime(now) {
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let h5 = document.querySelector("h5");
+  h5.innerHTML = `${days[now.getDay()]},  ${hours}:${minutes}`;
+  return h5.innerHTML;
+}
+currentTime(now);
+
+//Search-Function + 1 Temp-Function
+
+function getCurrentWeather(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let h7 = document.querySelector("h7");
+  h7.innerHTML = `${temperature}ºC`;
+  let city = response.data.name;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${city}`;
+  let dispcription = document.querySelector("#discription");
+  dispcription.innerHTML = response.data.weather[0].main;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = response.data.wind.speed + " km/s";
+  let precip = document.querySelector("#precip");
+  precip.innerHTML = response.data.main.humidity + "%";
+}
+function searchCity(city) {
+  let apiKey = "c6da6d296757d783639131d01c953a9f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getCurrentWeather);
+}
+
+function doSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", doSubmit);
+
+//Temperature-Functions
+
+function showCurrentPosition(position) {
+  let apiKey = "c6da6d296757d783639131d01c953a9f";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getCurrentWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showCurrentPosition);
+}
+let form1 = document.querySelector("#currentButton");
+form1.addEventListener("click", getCurrentLocation);
